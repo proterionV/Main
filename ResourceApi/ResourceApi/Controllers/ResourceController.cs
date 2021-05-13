1c049faccf902e4eab9cb8c225a12178bd737f78
proterion3@outlook.com <proterion3@outlook.com>
2021-05-13 19:00:09 +0400

    Добавил Api для получения, удаления изменения словаря

---
+++ ResourceApi/ResourceApi/Controllers/ResourceController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResourceApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace ResourceApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ResourceController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Resource> Get()
+        {
+            return DataItem.Items.Values.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Resource Get(int id)
+        {
+            return DataItem.Items.Values.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Resource resource)
+        {
+            if (DataItem.AddItem(resource))
+            {
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Resource resource)
+        {
+            if (DataItem.UpdateItem(resource))
+            {
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (DataItem.RemoveItem(id))
+                return Ok();
+
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ ResourceApi/ResourceApi/Models/DataItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceApi.Models
+{
+    public static class DataItem
+    {
+        public static ConcurrentDictionary<decimal, Resource> Items { get; set; } = new ConcurrentDictionary<decimal, Resource>();
+
+        static DataItem()
+        {
+            ItemsFillDefault();
+        }
+
+        private static void ItemsFillDefault() // заполняем словарь данными по умолчанию 
+        {
+            for (int i = 1; i < 5; i++)
+                Items.TryAdd(i, new Resource { Id = i, Name = $"Name {i}" });
+        }
+
+        public static bool AddItem(Resource Resource) // Добавим новый ресурс
+        {
+            if (Items.ContainsKey(Resource.Id))
+                return false;
+
+            return Items.TryAdd(Resource.Id, Resource);
+        }
+
+        public static bool RemoveItem(decimal Key) // удаляем ресурс
+        {
+            return Items.TryRemove(Key, out _);
+        }
+
+        public static bool UpdateItem(Resource Resource) // обновляем ресурс
+        {
+            if (Items.ContainsKey(Resource.Id))
+            {
+                Items[Resource.Id] = Resource;
+                return true;
+            }
+            return false;
+        }
+    }
+}
