0fbee900eccab0fba9175bf61663d738c36ec884
proterion3@outlook.com <proterion3@outlook.com>
2021-05-16 17:51:40 +0400

    + logs

--- ResourceApi/ResourceApi/Controllers/ResourceController.cs
+++ ResourceApi/ResourceApi/Controllers/ResourceController.cs
@@ -3,6 +3,7 @@ using ResourceApi.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System.Diagnostics;
+using Microsoft.Extensions.Logging;
 
 namespace ResourceApi.Controllers
 {
@@ -10,43 +11,51 @@ namespace ResourceApi.Controllers
     [Route("[controller]")]
     public class ResourceController : ControllerBase
     {
-        public ResourceController()
-        {
+        private readonly ILogger<ResourceController> _logger;
 
+        public ResourceController(ILogger<ResourceController> logger)
+        {
+            _logger = logger;
         }
 
         #region requsts
         [HttpGet]
         public IEnumerable<Resource> Get()
         {
-            Trace.TraceInformation("Запрос всех объектов словаря");
+            _logger.LogInformation("Запрос всех объектов словаря");
             return DataItem.Items.Values.ToList();
         }
 
         [HttpGet("{id}")]
         public Resource Get(int id)
         {
-            Trace.TraceInformation($"Запрос объекта {id} словаря");
+            _logger.LogInformation($"Запрос объекта {id} словаря");
             return DataItem.Items.Values.FirstOrDefault(x => x.Id == id);
         }
 
         [HttpPost]
         public IActionResult Post([FromBody] Resource resource)
         {
-            if (DataItem.AddItem(resource))
+            if (DataItem.Items.TryAdd(resource.Id, resource))
             {
+                _logger.LogInformation($"Оъект \"{resource.Id}\" добавлен");
                 return Ok();
             }
+
+            _logger.LogError($"Не удалось добавить объект \"{resource.Id}\"");
             return BadRequest(ModelState);
         }
 
         [HttpPut]
         public IActionResult Put([FromBody] Resource resource)
         {
-            if (DataItem.UpdateItem(resource))
+            if (DataItem.Items.TryUpdate(resource.Id, resource, DataItem.Items[resource.Id]))
             {
+                _logger.LogInformation($"Оъект \"{resource.Id}\" обновлен");
                 return Ok();
             }
+
+            _logger.LogError($"Невозможно обновить ресурс. {resource.Id}");
             return BadRequest(ModelState);
         }
 
@@ -55,11 +64,11 @@ namespace ResourceApi.Controllers
         {
             if (DataItem.Items.TryRemove(id, out _))
             {
-                Trace.TraceInformation($"Оъект \"{id}\" успешно удален");
+                _logger.LogInformation($"Оъект \"{id}\" удален");
                 return Ok();
             }
 
-            Trace.TraceError($"Не удалось удалить объект \"{id}\"");
+            _logger.LogError($"Не удалось удалить объект \"{id}\"");
             return BadRequest(ModelState);
         }
         #endregion
--- ResourceApi/ResourceApi/Models/DataItem.cs
+++ ResourceApi/ResourceApi/Models/DataItem.cs
@@ -23,47 +23,5 @@ namespace ResourceApi.Models
             for (int i = 1; i < 5; i++)
                 Items.TryAdd(i, new Resource { Id = i, Name = $"Name {i}" });
         }
-
-        /// <summary>
-        /// Добавление одного элемента в словарь
-        /// </summary>
-        /// <param name="Resource">Добавляемый объект</param>
-        /// <returns></returns>
-        public static bool AddItem(Resource Resource) 
-        {
-            if (Items.ContainsKey(Resource.Id))
-            {
-                Trace.TraceError($"Невозможно добавить ресурс, объект с идентификатором \"{Resource.Id}\" уже существует");
-                return false;
-            }
-
-            Trace.TraceInformation($"Оъект \"{Resource.Id}\" добавлен");
-            return Items.TryAdd(Resource.Id, Resource);
-        }
-
-        /// <summary>
-        /// Обновляем инфо о ресурсе
-        /// </summary>
-        /// <param name="Resource">Объект обновляемого ресурса</param>
-        /// <returns></returns>
-        public static bool UpdateItem(Resource Resource) 
-        {
-            try
-            {
-                if (Items.ContainsKey(Resource.Id))
-                {
-                    Items[Resource.Id] = Resource;
-                    Trace.TraceInformation($"Оъект \"{Resource.Id}\" обновлен");
-                    return true;
-                }
-                Trace.TraceError($"Невозможно обновить ресурс, объект с идентификатором \"{Resource.Id}\" не существует");
-                return false;
-            }
-            catch(Exception ex)
-            {
-                Trace.TraceError($"Невозможно обновить ресурс. {ex.Message}");
-                return false;
-            }
-        }
     }
 }
