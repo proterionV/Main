ab0d21afa74201a140df6196f4f246df81d43cf3
proterion3@outlook.com <proterion3@outlook.com>
2021-06-17 15:45:27 +0400

    fix Console Input

--- ResourceApi/ConsoleApp/Program.cs
+++ ResourceApi/ConsoleApp/Program.cs
@@ -15,30 +15,65 @@ namespace ConsoleApp
         static void Main()
         {
             #region ввод с консоли 
-            Console.Write("Object Count: ");
-            int maxObjects = int.Parse(Console.ReadLine()); // количество объектов
-
-            Console.Write("Thread Count: ");
-            int maxThreads = int.Parse(Console.ReadLine()); // количество потоков
-
-            Console.Write("Load Length: ");
-            int maxSteps = int.Parse(Console.ReadLine()); // количество итераций
+            int maxObjects = 0, maxThreads = 0, maxSteps = 0;
+            Input("Objects count", ref maxObjects); // количество объектов 
+            Input("Thread Count", ref maxThreads);// количество потоков
+            Input("Load Length", ref maxSteps);// количество шагов
             #endregion
 
-            ConsoleInit(maxSteps, maxObjects); 
+            try
+            {
+                ConsoleInit(maxSteps, maxObjects);
 
-            // создание последовательности экземпляров c инициализацией начальных положений в конструкторе
-            var works = Enumerable.Range(0, maxObjects).Select(index => new Work(0, index));
+                // создание последовательности экземпляров c инициализацией начальных положений в конструкторе
+                var works = Enumerable.Range(0, maxObjects).Select(index => new Work(0, index));
 
-            // выполнение метода обработчика в отдельном потоке для каждого объекта с ожиданием завершения каждого потока
-            Parallel.ForEach(works, new ParallelOptions { MaxDegreeOfParallelism = maxThreads }, work => Handler(work, maxSteps));
+                // выполнение метода обработчика в отдельном потоке для каждого объекта с ожиданием завершения каждого потока
+                Parallel.ForEach(works, new ParallelOptions { MaxDegreeOfParallelism = maxThreads }, work => Handler(work, maxSteps));
 
-            // вывод сообщения о завершении обработки объектов на позицию следующей строки после последнего результата
-            Console.SetCursorPosition(0, maxObjects);
-            Console.WriteLine("All Done!");
+                // вывод сообщения о завершении обработки объектов на позицию следующей строки после последнего результата
+                Console.SetCursorPosition(0, maxObjects);
+                Console.WriteLine("All Done!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Ввод и валидация входных параметров
+        /// </summary>
+        /// <param name="Name">Имя параметра</param>
+        /// <param name="parameter">Ссылка на параметр</param>
+        private static void Input(string Name, ref int parameter)
+        {
+            Console.Write($"{Name}: ");
+
+            while (!int.TryParse(Console.ReadLine(), out parameter))
+            {
+                Console.WriteLine("Incorrect value!");
+                Console.Write($"{Name}: ");
+            }
+        }
+
+        /// <summary>
+        /// Установка свойст окна консоли.
+        /// </summary>
+        /// <param name="width">Ширина окна консоли</param>
+        /// <param name="height">Высота окна консоли</param>
+        private static void ConsoleInit(int width, int height)
+        {
+            Console.Clear(); // очистка окна консоли, позиция курсора по умолчанию
+
+            Console.CursorVisible = false; // отключение видимости курсора
+
+            Console.ForegroundColor = ConsoleColor.Green; // цвет выводящейся информации
+
+            Console.SetWindowSize(width + 30, height + 10); // размер окна консоли (константные значения произвольные)
         }
 
         /// <summary>
@@ -51,7 +86,7 @@ namespace ConsoleApp
             sw.Start(); // запуск таймера
 
             // поскольку класс Console является статическим, для операций с консолью необходимо использовать блокировку для позиционирования курсора
-            lock (locker) 
+            lock (locker)
             {
                 // вывод информации об объекте и текущем потоке
                 Console.SetCursorPosition(work.X, work.Y);
@@ -71,10 +106,10 @@ namespace ConsoleApp
                     if (work.Chance > 8) throw new Exception($"Случайное исключение в потоке {Environment.CurrentManagedThreadId}, шаг {work.X}");
 
                     // если нет исключения - блокировка для вывода итерации цветом по умолчанию
-                    lock (locker) 
+                    lock (locker)
                     {
                         Console.SetCursorPosition(work.X, work.Y);
-                        Console.Write($"■ {work.X - startPos}");  
+                        Console.Write($"■ {work.X - startPos}");
                     }
                 }
                 catch (Exception ex)
@@ -101,25 +136,9 @@ namespace ConsoleApp
                 Console.SetCursorPosition(work.X, work.Y);         // позиционирование
                 Console.Write($" {work.X - startPos}  ");          // расчет текущей итерации
                 Console.ForegroundColor = ConsoleColor.Red;        // смена цвета вывода
-                Console.Write($"{queue.Count}! ({(double)sw.ElapsedMilliseconds/1000})"); // вывод позиции завершенной имитации, времени выполнения в секундах
+                Console.Write($"{queue.Count}! ({Math.Round((double)sw.ElapsedMilliseconds / 1000, 2)})"); // вывод позиции завершенной имитации, времени выполнения в секундах
                 Console.ForegroundColor = ConsoleColor.Green;      // исходный цвет вывода 
             }
-        }
-
-        /// <summary>
-        /// Установка свойст окна консоли.
-        /// </summary>
-        /// <param name="width">Ширина окна консоли</param>
-        /// <param name="height">Высота окна консоли</param>
-        private static void ConsoleInit(int width, int height)
-        {
-            Console.Clear(); // очистка окна консоли, позиция курсора по умолчанию
-
-            Console.CursorVisible = false; // отключение видимости курсора
-
-            Console.ForegroundColor = ConsoleColor.Green; // цвет выводящейся информации
-
-            Console.SetWindowSize(width + 50, height + 25); // размер окна консоли (константные значения произвольные)
         }
     }
 }
